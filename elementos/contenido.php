f39bfb11058a19ffb604aa03bce06e1bfacdfc82
SergioCamposDelgado <campos.delgado.sergio@iescamas.es>
2025-10-06 09:59:14 +0200

    Añadido otro producto

--- elementos/contenido.php
+++ elementos/contenido.php
@@ -1,7 +1,7 @@
 <h2 class="text-success text-center mt-4">Productos locales de Camas</h2>
 <ul class="list-group">
   <?php
-  $productos = ["Pan de Camas", "Aceitunas aliñadas", "Tortas de aceite"];
+  $productos = ["Pan de Camas", "Aceitunas aliñadas", "Tortas de aceite","Manzanas verdes"];
   foreach ($productos as $p) {
     echo "<li class='list-group-item'>" . htmlspecialchars($p) . "</li>";
   }
