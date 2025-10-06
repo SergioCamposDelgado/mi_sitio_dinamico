1cecab57c4048d1b79933a40257b3d1daad840ea
SergioCamposDelgado <campos.delgado.sergio@iescamas.es>
2025-10-06 10:10:12 +0200

    Añadida barra de navegación Bootstrap

--- elementos/header.php
+++ elementos/header.php
@@ -1,4 +1,28 @@
 <header class="text-center bg-white p-3 rounded shadow-sm">
   <h1 class="text-primary">🌟 Bienvenido a mi primer sitio PHP 🌟</h1>
   <p class="text-muted">Usando include() por primera vez</p>
+  <nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">Barra de navegación</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Inicio</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Precios</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Tiendas</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Patrocinadores</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
 </header>
